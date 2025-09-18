398a3700d1a0363a602b9f0aa03a4de0edfa3836
Anish Upreti <upretianish407@.com>
2025-09-18 14:33:07 +0545

    Connected to compass

--- config/database-connection.js
+++ config/database-connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose")
 const config = require("config")
 const connectDB = () =>{
-    return mongoose.connect(`${config.get("MONGODB_URI")}/messageMe`)
+   return  mongoose.connect(config.get("MONGODB_URI"))
+
 }
 
 module.exports = connectDB
